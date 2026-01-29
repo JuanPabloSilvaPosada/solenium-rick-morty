3fc401a3cb86afcbfbc7ef06ce47834cc6a4598e
Juan Silva <juanpablosilvaposada@gmail.com>
2026-01-29 11:07:59 -0500

    feat: add useCharacterSearch hook for api search

---
+++ src/hooks/useCharacterSearch.js
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import { getCharacters } from "../services/fetchingService";
+
+// Hook para buscar personajes de la API de Rick and Morty
+export const useCharacterSearch = () => {
+    // Estados para la búsqueda
+    const [characters, setCharacters] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+    // Variables para manejar la paginación
+    const [info, setInfo] = useState(null);
+    const [currentPage, setCurrentPage] = useState(1);
+
+    const searchCharacters = async (params = {}) => {
+        // Cambiar estados y limpiar errores
+        setLoading(true);
+        setError(null);
+        setCharacters([]);
+
+        try {
+            const data = await getCharacters(params);
+
+            setInfo(data.info);
+            setCurrentPage(params.page || 1);
+
+            if(data.results.length === 0) {
+                setError('No se encontraron personajes');
+            } else {
+                setCharacters(data.results);
+            }
+        } catch (error) {
+            setError(error.message);
+        } finally {
+            // Cambiar estado de loading a false
+            setLoading(false);
+        }
+    }
+
+    // Devolver los valores del hook
+    return {
+        characters,
+        loading,
+        error,
+        info,
+        currentPage,
+        searchCharacters,
+    }
+}
