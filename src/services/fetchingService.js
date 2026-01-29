431c363c1b5d6c94ce020cd675ace1d143844b46
Juan Silva <juanpablosilvaposada@gmail.com>
2026-01-29 11:05:39 -0500

    chore: rename api variable to fix reference error

--- src/services/fetchingService.js
+++ src/services/fetchingService.js
@@ -1,14 +1,19 @@
-const RickAndMortyApuURL = 'https://rickandmortyapi.com/api/character';
+// URL de la API de Rick and Morty
+const RickAndMortyApiURL = 'https://rickandmortyapi.com/api/character';
 
 export const getCharacters = async (params = {}) => {
+    // Petición a la API
     try {
-        const response = await fetch(`${RickAndMortyApuURL}/?${new URLSearchParams(params)}`);
+        const response = await fetch(`${RickAndMortyApiURL}/?${new URLSearchParams(params)}`);
+        // Si la respuesta es 404, retorna un array vacío
         if (response.status === 404) {
             return { results: [], info: null }
         }
+        // Si la respuesta no es ok, lanza un error
         if (!response.ok) {
             throw new Error('Error al conectar con la API');
         }
+        // Se obtienen los datos de response e info (para paginación)
         const data = await response.json();
         return data;
     } catch (error) {
